89578898bf8a0b0083e62eaec9e04f27954dcc75
saffet502 <83337381+saffet502@users.noreply.github.com>
2021-06-06 01:14:38 +0200

    Update DenemeDort.java

--- src/DenemeDort.java
+++ src/DenemeDort.java
@@ -1,5 +1,6 @@
 public class DenemeDort {
     public static void main(String[] args) {
         System.out.println("DenemeDort");
+         System.out.println("DenemeDort icin tesekkurler");
     }
 }
